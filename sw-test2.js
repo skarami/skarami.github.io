2420d7dfb7574384d1540982dc4429b7ec80d666
Soroush <skaram5@uic.edu>
2020-01-29 13:25:51 -0600

    Update sw-test2.js

--- sw-test2.js
+++ sw-test2.js
@@ -1,12 +1,10 @@
 self.addEventListener('fetch', function(event) {
 
-  if (event.request.url.includes('?test')){
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
-});
-} 
-}
+    if (event.request.url.includes('?test')){
+        event.respondWith(
+            caches.match(event.request).then(function(response) {
+                return response || fetch(event.request);
+            })
+        );
+    }
 });
